25295cec509dea18d7b4e17214a5789ccb2a07e7
Василий Метт <mettvd@sibintek.ru>
2023-10-18 19:12:55 +0300

    Удаление трейтов для перечислений

--- src/Traits/Enum/BaseEnumTrait.php
+++ src/Traits/Enum/BaseEnumTrait.php
@@ -11,26 +11,6 @@ use Laravel\Foundation\Exceptions\EnumTransNotFoundException;
  */
 trait BaseEnumTrait
 {
-    static string $langFileName = 'enums';
-
-    /**Получить название enum из lang.enums.[class_name]
-     * @param array $args
-     * @return string
-     * @throws EnumTransNotFoundException
-     */
-    public function trans(array $args = []): string
-    {
-        return trans(self::getTransId($this->value), $args);
-    }
-
-    /**список всех значений этого enum
-     * @return array<string>
-     */
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
-
     /**Список всех значений с переводами
      * @param array<array> $args массив параметров для подстановки
      * @return Collection
@@ -45,15 +25,49 @@ trait BaseEnumTrait
             ]);
     }
 
+    /**список всех значений этого enum
+     * @return array<string>
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
     /**
      * @throws EnumTransNotFoundException
      */
     private static function getTransId(string $code): string
     {
-        $key = self::$langFileName . '.' . static::class . '.' . $code;
+        $key = 'enums.' . static::class . '.' . $code;
         if (trans()->has($key)) {
             return $key;
         }
         throw new EnumTransNotFoundException('Для класса ' . static::class . ' не найден перевод ' . $key);
+    }
+
+    /**Получить название enum из lang.enums.[class_name]
+     * @param array $args
+     * @return string
+     * @throws EnumTransNotFoundException
+     */
+    public function trans(array $args = []): string
+    {
+        return trans(self::getTransId($this->value), $args);
+    }
+
+    /**Получение объекта перечисления по его переводу
+     * @param string $translation
+     * @return \UnitEnum
+     * @throws \Exception
+     */
+    public static function fromTrans(string $translation): self
+    {
+        foreach (trans('enums.' . static::class) as $key => $trans) {
+            if ($trans === $translation) {
+                return self::from($key);
+            }
+        }
+
+        throw new \Exception('Не найден ключ по переводу ' . $translation);
     }
 }
